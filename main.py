b67f12903117e10094ea268aa1b8f282779a71c9
Unionark <1983zvd@gmail.com>
2026-09-14 22:01:06 +0300

    fix code

---
+++ main.py
@@ -0,0 +1,18 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+
+API_TOKEN = '8595642966:AAG4eVdxKpeISbuQIJzIG0EfweFeNB-V5Xg'
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Привет! Я спортивный бот. Чем могу помочь?")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
