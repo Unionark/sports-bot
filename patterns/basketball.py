26a28a061e865afc19d4c7c2ec654ef274e6d60c
Unionark <1983zvd@gmail.com>
2026-09-16 19:33:04 +0300

    add basketball pattern: favorite wins Q1 -> X2 underdog Q2

---
+++ patterns/basketball.py
@@ -0,0 +1,15 @@
+basketball_patterns = {
+    "favorite_wins_q1_underdog_x2_q2": {
+        "sport": "basketball",
+        "conditions": {
+            "favorite_odds_min": 1.1,
+            "favorite_odds_max": 1.5,
+            "favorite_won_q1": True
+        },
+        "prediction": "X2",
+        "prediction_target": "underdog",
+        "prediction_quarter": 2,
+        "message": "Фаворит выиграл первую четверть. Прогноз: X2 на аутсайдера во второй четверти."
+    }
+}
+
