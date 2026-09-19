afdc23fe1c7e7c2a14c009b1a50e20bd94d0be99
Unionark <1983zvd@gmail.com>
2026-09-19 18:16:55 +0300

    feat: add yellow cards strategy module and integrate into main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pattern_in_it import patterns
 from basketball import basketball_patterns
 from .hockey import hockey_patterns
 from patterns.volleyball_pattern import check_volleyball_pattern
+from patterns.yellow_cards.yellow_cards_strategy import evaluate_yellow_card_pattern
 
 API_TOKEN = '8595642966:AAG4eVdxKpeISbuQIJzIG0EfweFeNB-V5Xg'
 
